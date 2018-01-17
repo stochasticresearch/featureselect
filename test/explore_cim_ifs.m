54d20c2787781129982c185a4d570a80c79bfef1
Kiran Karra <kiran.karra@gmail.com>
2018-01-17 14:16:15 -0500

    minor updates for different testing

--- test/explore_cim_ifs.m
+++ test/explore_cim_ifs.m
@@ -11,7 +11,7 @@ else
     folder = '/home/kiran/ownCloud/PhD/sim_results/feature_select_challenge';
 end
 
-dataset = 'arcene';
+dataset = 'dexter';
 
 % load the data
 load(fullfile(folder,dataset,'data.mat'));
@@ -19,7 +19,7 @@ X = double(X_train);
 y = double(y_train);
 
 % load IFS for TAU
-miEstimator = 'tau';
+miEstimator = 'taukl';
 dataFname = strcat(dataset,'_ifs_',miEstimator,'.mat');
 fPath = fullfile(folder,dataset,dataFname);
 load(fPath);
@@ -32,14 +32,14 @@ fPath = fullfile(folder,dataset,dataFname);
 load(fPath);
 tCim = t;
 
-subplot(2,3,1); plot(tTau); title('\tau'); 
+subplot(2,3,1); plot(tTau); title('\tau_{KL}'); 
 subplot(2,3,2); plot(tCim); title('CIM');
-subplot(2,3,3); plot(abs(tTau)-abs(tCim)); title('|\tau|-|CIM|');
-subplot(2,3,4); plot(dep2mi(tTau)); title('MI(\tau)');
+subplot(2,3,3); plot(abs(tTau)-abs(tCim)); title('|\tau_{KL}|-|CIM|');
+subplot(2,3,4); plot(dep2mi(tTau)); title('MI(\tau_{KL})');
 subplot(2,3,5); plot(dep2mi(tCim)); title('MI(cim)');
-subplot(2,3,6); plot(dep2mi(tTau)-dep2mi(tCim)); title('MI(\tau)-MI(CIM)');
+subplot(2,3,6); plot(dep2mi(tTau)-dep2mi(tCim)); title('MI(\tau_{KL})-MI(CIM)');
 
-%% explore why CIM is producing 0.5 consantly for several indices in the arcene dataset!
+%% explore why CIM values for several interesting indices in the arcene dataset!
 
 clear;
 clc;
@@ -52,18 +52,20 @@ else
     folder = '/home/kiran/ownCloud/PhD/sim_results/feature_select_challenge';
 end
 
-dataset = 'arcene';
+dataset = 'dexter';
 
 % load the data
 load(fullfile(folder,dataset,'data.mat'));
 X = double(X_train);
 y = double(y_train);
 
-% load IFS for CIM
-miEstimator = 'cim';
-dataFname = strcat(dataset,'_ifs_',miEstimator,'.mat');
-fPath = fullfile(folder,dataset,dataFname);
-load(fPath);
-tCim = t;
+idxOfInterest = 626;
 
-xIdxOfInterest = find(tCim==0.5);
+xx = X(:,idxOfInterest);
+yy = y;
+
+[u,v] = pobs_sorted_cc_mex(xx,yy); 
+
+abs(taukl_cc(u,v,0,1,0))
+abs(taukl_cc(v,u,0,1,0))
+cim_v2_hybrid_cc(u,v,0.015625,0.2)
